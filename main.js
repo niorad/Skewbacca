7560b741ae0e17c38a3347e05c72039da8fe73de
niorad <mail@niorad.com>
2018-12-07 16:04:58 +0100

    Initial Commit

---
+++ main.js
@@ -0,0 +1,84 @@
+const { app, BrowserWindow, dialog } = require("electron");
+const { exec } = require("child_process");
+
+let mainWindow;
+
+const getFileFromUser = (exports.getFileFromUser = () => {
+  const files = dialog.showOpenDialog(mainWindow, {
+    properties: ["openFile"]
+  });
+  if (!files) {
+    return;
+  }
+  const file = files[0];
+  openFile(file);
+  console.log(file);
+});
+
+const openFile = file => {
+  const content = "a";
+  mainWindow.webContents.send("file-opened", file, content);
+};
+
+const convert = (exports.convert = (
+  sourceFileName,
+  targetFileName,
+  coords,
+  nw,
+  nh
+) => {
+  console.log(
+    generateConversionCommand(sourceFileName, targetFileName, coords, nw, nh)
+  );
+  exec(
+    generateConversionCommand(sourceFileName, targetFileName, coords, nw, nh),
+    (err, stdout, stderr) => {
+      console.log(stdout);
+      console.log(stderr);
+      mainWindow.webContents.send("file-saved", targetFileName);
+    }
+  );
+});
+
+function generateConversionCommand(
+  sourceFileName,
+  targetFileName,
+  c,
+  natWidth,
+  natHeight
+) {
+  const longerSide = Math.max(natWidth, natHeight);
+  const height = (longerSide / 100) * 69;
+  return `convert ${sourceFileName} \\( +clone -rotate 90 +clone -mosaic +level-colors white \\) +swap -gravity Northwest -composite -distort Perspective '${
+    c.TLX
+  },${c.TLY} 0,0 ${c.BLX},${c.BLY} 0,${height}  ${c.BRX},${
+    c.BRY
+  } ${longerSide},${height}  ${c.TRX},${
+    c.TRY
+  } ${longerSide},0' -gravity Northwest -crop ${longerSide}x${height}+0+0 +repage ${targetFileName}`;
+}
+
+function createWindow() {
+  mainWindow = new BrowserWindow({ width: 500, height: 500 });
+  mainWindow.loadFile("index.html");
+  mainWindow.setResizable(false);
+
+  mainWindow.webContents.openDevTools();
+  mainWindow.on("closed", function() {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", function() {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", function() {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
---
+++ renderer.js
@@ -0,0 +1,75 @@
+const { remote, ipcRenderer } = require("electron");
+const mainProcess = remote.require("./main.js");
+
+const handles = document.querySelectorAll(".handle");
+const handleWrapper = document.getElementById("handle-wrapper");
+let openFile = "";
+
+document.getElementById("open").addEventListener("click", () => {
+  mainProcess.getFileFromUser();
+});
+
+document.getElementById("save").addEventListener("click", () => {
+  const nw = document.getElementById("main-image").naturalWidth;
+  const nh = document.getElementById("main-image").naturalHeight;
+  mainProcess.convert(
+    openFile,
+    "/Users/antonioradovcic/Desktop/hmpf.jpg",
+    getSkewCoordinates(),
+    nw,
+    nh
+  );
+});
+
+ipcRenderer.on("file-opened", (event, file, content) => {
+  console.log(file);
+  openFile = file;
+  document.getElementById("main-image").src = file;
+  handleWrapper.style.display = "block";
+});
+
+ipcRenderer.on("file-saved", (event, targetFileName) => {
+  console.log(targetFileName);
+  document.getElementById("preview-image").src = targetFileName;
+});
+
+handles.forEach(handle => {
+  handle.addEventListener("dragend", e => {
+    if (!e.pageX && !e.pageY) return;
+    e.target.style.top = e.pageY + "px";
+    e.target.style.left = e.pageX + "px";
+  });
+  handle.addEventListener("drag", e => {
+    if (!e.pageX && !e.pageY) return;
+    e.target.style.top = e.pageY + "px";
+    e.target.style.left = e.pageX + "px";
+  });
+  handle.addEventListener("dragstart", e => {
+    const img = document.createElement("span");
+    img.style.display = "none";
+    e.dataTransfer.setDragImage(img, 0, 0);
+  });
+});
+
+function getSkewCoordinates() {
+  const TLX = percX(document.getElementById("topleft").offsetLeft);
+  const TLY = percY(document.getElementById("topleft").offsetTop);
+  const TRX = percX(document.getElementById("topright").offsetLeft);
+  const TRY = percY(document.getElementById("topright").offsetTop);
+  const BRX = percX(document.getElementById("bottomright").offsetLeft);
+  const BRY = percY(document.getElementById("bottomright").offsetTop);
+  const BLX = percX(document.getElementById("bottomleft").offsetLeft);
+  const BLY = percY(document.getElementById("bottomleft").offsetTop);
+  return { TLX, TLY, TRX, TRY, BRX, BRY, BLX, BLY };
+}
+
+function percX(n) {
+  const nat = document.getElementById("main-image").naturalWidth;
+  return (n / handleWrapper.getBoundingClientRect().width) * nat;
+}
+
+function percY(n) {
+  const nat = document.getElementById("main-image").naturalHeight;
+  console.log(n, handleWrapper.getBoundingClientRect().height, nat);
+  return (n / handleWrapper.getBoundingClientRect().height) * nat;
+}
