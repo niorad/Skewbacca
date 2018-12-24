3353536ce73f8ce9bb4236a5357adabee139ba11
niorad <mail@niorad.com>
2018-12-24 13:31:05 +0100

    Module-Foo and Tests

--- test/imageConverterCommands.ts
+++ test/imageConverterCommands.ts
@@ -3,7 +3,7 @@ import "mocha";
 import * as assert from "assert";
 
 describe("Image Converter", function() {
-  it("Generates a correct image-conversion-command for ImageMagick", function() {
+  it("generates a correct unskewing-command for ImageMagick", function() {
     const imageConverter = new ImageConverter();
 
     const unskewCommand = imageConverter.generateUnskewCommand(
@@ -25,5 +25,16 @@ describe("Image Converter", function() {
 
     const idealCommand = `convert 'sourceFile' \\( +clone -rotate 90 +clone -mosaic +level-colors white \\) +swap -gravity Northwest -composite -distort Perspective '10,10 0,0 10,10 0,690  10,10 1000,690  10,10 1000,0' -gravity Northwest -crop 1000x690+0+0 +repage 'targetFile'`;
     assert.equal(unskewCommand, idealCommand);
+  });
+
+  it("generates a correct resizing-command for ImageMagick", function() {
+    const imageConverter = new ImageConverter();
+    const resizeCommand = imageConverter.generateResizeCommand(
+      "test.jpg",
+      10,
+      "dest.jpg"
+    );
+    const idealCommand = `convert 'test.jpg' -resize 10% 'dest.jpg'`;
+    assert.equal(resizeCommand, idealCommand);
   });
 });
